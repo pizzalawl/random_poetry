81d0077f5663286b7c8a7b696e11d8d521ccfd85
pizzalawl <pizzalawls@gmail.com>
2022-05-09 13:06:42 -0500

    organizing files + more decorations

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let url = 'https://poetrydb.org/random,linecount/1;10/title,author,lines.json'
 const button = document.getElementById('button')
 const title = document.getElementById('title')
 const author = document.getElementById('author')
+const fullBody = document.getElementById('fullbody')
 const element = document.getElementById('element')
 
 
@@ -11,7 +12,7 @@ async function requestPoem(url) {
   return data
 }
   
-button.onclick = async () => {
+myButton.onclick = async () => {
   let data = await requestPoem(url)
   let poem = data[0].lines
   title.innerHTML = data[0].title
