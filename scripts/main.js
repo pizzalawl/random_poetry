321338fe18875b8c97eaaa6b6706dea76335afbb
pizzalawl <pizzalawls@gmail.com>
2022-05-13 15:24:12 -0500

    fix issues with functions(yay it's done)

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,7 @@ const title = document.getElementById('title')
 const author = document.getElementById('author')
 const container = document.getElementById('container')
 const element = document.getElementById('element')
-const form = document.getElementById("form")
-let val = document.getElementById('lineCount').value
+let val = document.getElementById("input").value
 
 async function requestPoem(url) {
   let response = await fetch(url);
@@ -16,30 +15,28 @@ function clearContainer() {
   container.innerHTML = ""
 }
 
-function checkFormValue(val) {
+function checkFormValue() {
+  val = document.getElementById("input").value
   if (val == "") {
     val = "10"
   }
   return val
 }
 
-function setPoem(data, titleData, authorData) {
-  title.innerHTML = titleData
-  author.innerHTML = authorData
-  let poem = data.lines
+function setPoem(data) {
+  let poem = data[0].lines
+  title.innerHTML = data[0].title
+  author.innerHTML = data[0].author
   for (let i = 0; i < poem.length; i++) {
     let newElement = document.createElement('p')
     newElement.innerHTML = poem[i];
     container.appendChild(newElement)
-  }
-}
+} }
+
   
 myButton.onclick = async () => { 
   clearContainer()
-  let url = `https://poetrydb.org/random,linecount/1;${checkFormValue(val)}/title,author,lines.json`
+  let url = `https://poetrydb.org/random,linecount/1;${checkFormValue()}/title,author,lines.json`
   let data = await requestPoem(url)
-  setPoem(data[0], data[0].title, data[0].author)
+  setPoem(data)
 }
-
-
-
